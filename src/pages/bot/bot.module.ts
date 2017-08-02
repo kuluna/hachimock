885d9e6474f1fe93d8056839d118b67d000b2bf7
kuluna <mailkuluna@gmail.com>
2017-08-02 10:53:05 +0900

    Add assign dialog

--- src/pages/bot/bot.module.ts
+++ src/pages/bot/bot.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
+
 import { BotPage } from './bot';
 
 @NgModule({
@@ -9,5 +10,7 @@ import { BotPage } from './bot';
   imports: [
     IonicPageModule.forChild(BotPage),
   ],
+  providers: [
+  ]
 })
 export class BotPageModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AlertController, NavController } from 'ionic-angular';
 import { Http } from '@angular/http';
 
 import { BotPage } from '../bot/bot';
@@ -10,12 +10,32 @@ import { BotPage } from '../bot/bot';
 })
 export class HomePage {
   bots: Bot[] = [];
+  assignments = ['Ryota Sakaguchi', 'Taro Tanaka', 'Hanako Tanaka'];
+  assign = this.assignments[0];
 
 
-  constructor(public navCtrl: NavController, public http: Http) {
+  constructor(public navCtrl: NavController, public http: Http, public alert: AlertController) {
     this.http.get('assets/data/bots.json').subscribe(bots => {
       this.bots = bots.json() as Bot[];
     });
+  }
+
+  showAssignDialog() {
+    var dialog = this.alert.create();
+    dialog.setTitle('Assign');
+
+    this.assignments.forEach(name => {
+      dialog.addInput({ type: 'radio', label: name, value: name });
+    });
+    
+
+    dialog.addButton('Cancel');
+    dialog.addButton({ text: 'Assign', handler: (value) => {
+      this.assign = value;
+    }});
+
+    // show
+    dialog.present();
   }
 
   pushBotDetail() {
